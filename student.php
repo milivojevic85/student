28c14cb64547eab5986a88bc2a693aab2875e12b
milivojevic85 <misagrf@yahoo.com>
2020-01-27 12:03:55 +0100

    explaination added

--- student.php
+++ student.php
@@ -9,25 +9,34 @@
 
 class Student
 {
+	// adding class attributes or properties
 	public $first_name;
 	public $last_name;
 	public $address;
+	
+	// initializing our properties, the constructor method is automatically fired upon object instantiation
 	public function __construct($first_name, $last_name, $address) {
 		$this->first_name = $first_name;
 		$this->last_name = $last_name;
 		$this->address = $address;	
 	}
+	// a class method
 	public function greeting() {
 		return "Hello ".$this->first_name."<br>";
 	}
+	// a class method
 	public function getAddress() {
 		return $this->address."<br>";
 	}
 }
 
-$student1 = new Student("John", "Doe", "3235 Woodland Park St");
-echo $student1->greeting();
-echo $student1->getAddress();
+// object instantiation 
+$student1 = new Student("John", "Doe", "3235 Woodland Park St"); 
+//arguments are values assigned to the properties
+
+// accessing the class methods
+echo $student1->greeting(); // Hello John
+echo $student1->getAddress(); // 3235 Woodland Park St
 
 
 ?>
